b007a356bebd6e0c2d2f9e91f7c9ad4256f4b7a0
jacob-dinapoli <jacob.dinapoli@mymail.champlain.edu>
2020-10-09 21:03:15 +0000

    linux

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,7 @@
+#secure-ssh.sh
+#author jacob.dinapoli
+#creates a new shh user using $1 parameter
+#Adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
+
